81f60560c6092ea6c38a595c91fba1c81116b56b
Gray <timekidgray@gmail.com>
2024-12-30 20:08:16 +0100

    Adding sound

--- src/prefabs/AlarmTrigger.js
+++ src/prefabs/AlarmTrigger.js
@@ -1,45 +1,55 @@
 import Phaser from 'phaser';
 
 class AlarmTrigger extends Phaser.GameObjects.Rectangle {
-    constructor(scene, x, y, width = 100, height = 100) {
-        super(scene, x, y, width, height, 0xff0000, 0.3);
+    constructor(scene, x, y) {
+        super(scene, x, y);
         
-        this.scene = scene;
-        this.isTriggered = false;
-        this.alarmDuration = 3000; // Duration in milliseconds
-        this.triggerTime = 0;
+        // Add to scene
+        scene.add.existing(this);
         
-        // Set initial alpha
-        this.setAlpha(0.3);
+        // Set up graphics
+        this.setSize(32, 32);
+        
+        // Only show if added through Zucc menu
+        const fromZucc = scene.trapConfig && scene.trapConfig.AlarmTrigger > 0;
+        if (fromZucc) {
+            // Create a red rectangle for visibility
+            const graphics = scene.add.graphics();
+            graphics.lineStyle(2, 0xff0000);
+            graphics.strokeRect(-16, -16, 32, 32);
+            graphics.fillStyle(0xff0000, 0.3);
+            graphics.fillRect(-16, -16, 32, 32);
+            this.add(graphics);
+        } else {
+            // Make invisible if not from Zucc menu
+            this.setAlpha(0);
+        }
+
+        // Initialize sound
+        this.alarmSound = scene.sound.add('alarm', { loop: false });
+        this.triggered = false;
     }
 
     update() {
-        if (this.isTriggered && (this.scene.time.now - this.triggerTime >= this.alarmDuration)) {
+        if (this.triggered && (this.scene.time.now - this.triggerTime >= this.alarmDuration)) {
             this.deactivate();
         }
     }
 
-    trigger() {
-        if (!this.isTriggered) {
-            this.isTriggered = true;
-            this.triggerTime = this.scene.time.now;
-            this.setAlpha(0.8);
+    triggerAlarm() {
+        if (!this.triggered) {
+            this.alarmSound.play();
+            this.triggered = true;
             
-            // Play alarm sound if available
-            try {
-                if (this.scene.sound && this.scene.cache.audio.exists('alarm')) {
-                    const alarm = this.scene.sound.add('alarm', { volume: 0.5 });
-                    alarm.play();
-                }
-            } catch (error) {
-                console.warn('Could not play alarm sound:', error);
-            }
+            // Reset after sound duration
+            this.scene.time.delayedCall(this.alarmSound.duration * 1000, () => {
+                this.triggered = false;
+            });
         }
     }
 
     deactivate() {
-        this.isTriggered = false;
-        this.setAlpha(0.3);
+        this.triggered = false;
     }
 }
 
